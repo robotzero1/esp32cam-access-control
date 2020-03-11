fae21da82e4c4f76541bc6aea925ae341e83ccd6
robotzero1 <60509953+robotzero1@users.noreply.github.com>
2020-03-11 15:49:01 +0100

    Update FaceDoorEntryESP32Cam.ino

--- FaceDoorEntryESP32Cam.ino
+++ FaceDoorEntryESP32Cam.ino
@@ -8,8 +8,8 @@
 #include "fr_forward.h"
 #include "fr_flash.h"
 
-const char* ssid = "NSA HONEYPOT";
-const char* password = "only4andrew";
+const char* ssid = "NSA";
+const char* password = "Orange";
 
 #define ENROLL_CONFIRM_TIMES 5
 #define FACE_ID_SAVE_NUMBER 7
